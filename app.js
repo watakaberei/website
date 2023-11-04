279dd15f4489c8567c369efa821b78fd206ec02f
watakaberei <126738738+watakaberei@users.noreply.github.com>
2023-11-04 12:44:21 +0000

    中断イベントの利用

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
-const readline = require('readline');
+const gl = require("./getline.js")
 
-var counter = 0;
+const leadline = require('readline')
 
-const read = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+//メイン処理
+async function main() {
+    while(true) {
+        var re = await gl.getline('input: ');
+        console.log('(' + re.length + ' chars)');
+    }
+}
 
-read.on('line', (line) => {
-    counter++;
-    console.log('(' + counter + ': ' + line.length + ' chars)');
-});
-
-var content = ["--begin--",
-    "これは、長文のコンテンツです。",
-    "長い文章を出力させる例です。",
-    "どのように表示されるかな？",
-    "--end--"].join('\n');
-read.write(content);
-read.close();
+main(); //メインの処理
