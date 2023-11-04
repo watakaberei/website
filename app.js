f5cbf292ce3e0ae7bba58b3110cf2b7f8fb30d25
watakaberei <126738738+watakaberei@users.noreply.github.com>
2023-11-04 12:20:31 +0000

    改行イベントの利用

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const gl = require("./getline.js")
+const readline = require('readline');
 
-//メイン処理
-async function main() {
-    console.log(('--begin'));
-    while(true) {
-        var result = await gl.getline('type any words: ');
-        if (result == '') { break; }
-        console.log('you typed: ' + result);
-    }
-    console.log('--end--');
-}
+var counter = 0;
 
-main(); //メインの処理
+const read = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+read.on('line', (line) => {
+    counter++;
+    console.log('(' + counter + ': ' + line.length + ' chars)');
+});
+
+var content = ["--begin--",
+    "これは、長文のコンテンツです。",
+    "長い文章を出力させる例です。",
+    "どのように表示されるかな？",
+    "--end--"].join('\n');
+read.write(content);
+read.close();
